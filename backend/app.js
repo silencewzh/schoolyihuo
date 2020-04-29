adf78b62bca97f83650091143a207a7690b75b59
silencewzh <42534004+silencewzh@users.noreply.github.com>
2020-04-30 00:02:54 +0800

    add routes model

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var knowledgeRouter=require('./routes/knowledge');
+var welfareRouter=require('./routes/welfare');
+var goodsRouter=require('./routes/goods');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/knowledge',knowledgeRouter);
+app.use('/welfare',welfareRouter);
+app.use('/goods',goodsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
